efb64ee0453a69b7794883e1b33ff16e88960110
Chris Smith <theupstairs@hotmail.com>
2015-11-27 20:55:24 -0800

    updated HTML tag IDs, fixed script file name typo, created new JS fuctions

--- js/task-framework.js
+++ js/task-framework.js
@@ -11,3 +11,34 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+function addListItem () {
+	var liOrderNumber = 0;
+	var newTaskTitle = document.getElementById("add-task");
+	var ul = document.getElementById("task-list");
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(newItem));
+	li.setAttribute("id",liOrderNumber);
+	liOrderNumber++;
+	ul.appendChild(li);
+	alert(li.id);
+}
+
+function testFunction (e) {
+	var key = e.which || e.keycode || e.key;
+	if ( key == 13) {
+		window.alert("Enter key pressed");
+	}
+}
+
+function loadTaskFramework() {
+
+	// add the footer and confirm JScript is running
+    var feet = document.getElementsByTagName('footer');
+    feet[0].innerHTML = "Let's Do This";
+
+    // register an event handler for the new task input element
+	var objNewTask = document.getElementById("in-task-name");
+	objNewTask.addEventListener('keydown', testFunction, false);
+}
+onload = loadTaskFramework;
